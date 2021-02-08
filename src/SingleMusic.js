fad00af023b37ac687d6a76867e73a9a5c200ea0
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2021-02-08 20:18:41 +0100

    track

--- src/SingleMusic.js
+++ src/SingleMusic.js
@@ -53,7 +53,7 @@ const SingleMusic = () => {
         <h4>Album: {album.title}</h4>
         <h5>Duration: {duration} s</h5>
         <figure>
-            <figcaption>Preview the track:</figcaption>
+            <figcaption>Track preview :</figcaption>
             <audio
                 controls
                 src={preview}>
