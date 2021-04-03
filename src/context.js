9788d1ba2c80eeced1672b2431aa01649ee88616
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2021-04-03 13:45:34 +0200

    update to load all search result

--- src/context.js
+++ src/context.js
@@ -1,14 +1,10 @@
-import React, { useState, useContext } from 'react'
+import React, {useContext } from 'react'
 import useFetch from './useFetch'
-// export const API_ENDPOINT = "https://deezerdevs-deezer.p.rapidapi.com/search?rapidapi-key=0fb5807642mshdb70ba68e07c274p1ca20cjsna090e1309d86"
 const AppContext = React.createContext()
-
 const AppProvider = ({ children }) => {
-  const [query, setQuery] = useState('eminem')
-  const { isLoading, error, dataMusic } = useFetch(`&q=${query}`)
-
+  const {  isLoading,query,setQuery, setIndex, musics } = useFetch()
   return (
-    <AppContext.Provider value={{ isLoading, error, dataMusic, query, setQuery }}>
+    <AppContext.Provider value={{  isLoading,query,setQuery, setIndex, musics }}>
       {children}
     </AppContext.Provider>
   )
